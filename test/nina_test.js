59aa485ced3000cffe972e9b7601e63379a3f6c6
Christopher EnyTC <chris.enytc@gmail.com>
2013-12-10 06:24:08 -0500

    Upload v0.1.1

--- test/nina_test.js
+++ test/nina_test.js
@@ -30,7 +30,7 @@ exports['generate'] = {
   'no args': function(test) {
     test.expect(1);
     // tests here
-    test.equal(nina.generate(null, 'enytc.com', '198.168.0.1', 198), 'a6f8927d3bbca958e925f9060380226584fa7012f21aeb4718f90404ce528a0e0581d7b72b05980045504a3a41171817fc1e01bccfb5341315391f7f934270f8', 'should be return a hash.');
+    test.equal(nina.generate(null, 'example.com', '198.168.0.1', 199), '0d2edffa318d27f1d0d3c1479d6591bc1e0c8e6b44fec643b8c0e0dcf4b23b5a9d56e91a3a5883e62415d939da93484203278336b352adf1c364ef70b95e4891', 'should be return a hash.');
     test.done();
   },
 };
